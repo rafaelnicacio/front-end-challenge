256b484a1a6f4c6af436acdc3f9656356c40b58b
Rafael Nicacio <rafael.nicacio95@gmail.com>
2026-09-19 19:09:16 -0300

    feat: fix route for track-list.component

--- audiocode/src/app/app.routes.ts
+++ audiocode/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TrackListPageComponent } from './components/track-list/track-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: TrackListPageComponent }
+];
