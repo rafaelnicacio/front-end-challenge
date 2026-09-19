3ebe7ae9182a7d34eeed15d3d69a3604f0ad1034
Rafael Nicacio <rafael.nicacio95@gmail.com>
2026-09-19 18:39:29 -0300

    feat: create track list page

---
+++ audiocode/src/app/components/track-list/track-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './track-list.component.html',
+  styleUrl: './track-list.component.scss'
+})
+export class TrackListComponent {
+
+}
