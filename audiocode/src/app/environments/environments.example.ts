1cc566c708f50b8a4d62914e1df47c90923c8200
Rafael Nicacio <rafael.nicacio95@gmail.com>
2026-09-23 18:41:12 -0300

    feat: create environments.example

---
+++ audiocode/src/app/environments/environments.example.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  spotify: {
+    clientId: 'SEU_CLIENT_ID',
+    clientSecret: 'SEU_CLIENT_SECRET',
+    apiUrl: 'https://api.spotify.com/v1'
+  }
+};
